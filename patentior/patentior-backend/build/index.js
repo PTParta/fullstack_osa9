4db5a722661bc108e8b19095379cc0eae20b9c9e
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2021-02-24 13:23:08 +0200

    9.8-9.9 done

---
+++ patentior/patentior-backend/build/index.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require('dotenv').config();
+const cors = require('cors');
+const app = express_1.default();
+app.use(express_1.default.json());
+app.use(cors());
+const PORT = process.env.PORT || 3001;
+app.get('api/ping', (_req, res) => {
+    console.log('someone pinged here');
+    res.send('pong');
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
